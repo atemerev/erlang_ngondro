3d3651a626436fd00b4f3bca61f47c2128464e58
Namdak Tonpa <maxim@synrc.com>
2019-08-13 19:29:56 +0300

    mad rebar3 gun syn n2o

--- src/conn_manager.erl
+++ src/conn_manager.erl
@@ -1,13 +1,21 @@
 -module(conn_manager).
+-include_lib("n2o/include/n2o.hrl").
 -compile(export_all).
 
 proc(init, P) ->
-  {ok, Conn} = gun:open("www.bitmex.com", 443),
+  {ok, Conn} = gun:open("www.bitmex.com", 80),
   gun:ws_upgrade(Conn, "/realtime"),
   {ok, P};
 proc({gun_ws, _, _, Msg}, P) ->
-  io:format("~s~n", Msg),
+  io:format("TIC: ~p~n", [Msg]),
   {reply, [], P};
 proc({gun_down, _, _, Reason, _, _}, P) ->
-  io:format("Gun down: ~s~n", Reason),
+  io:format("TIC DOWN: ~p~n", [Reason]),
+  {reply, [], P};
+proc({gun_up, Reason, _}, P) ->
+  io:format("TIC UP: ~p~n", [Reason]),
+  {reply, [], P};
+proc(Unknown, P) ->
+  io:format("TIC UNK: ~p~n", [Unknown]),
   {reply, [], P}.
+
