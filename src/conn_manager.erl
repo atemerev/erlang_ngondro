e7b0f20c6498ed28945114dfe782c70d7d1b80a5
Alexander Temerev <sorhed@gmail.com>
2019-08-26 22:18:03 +0200

    Restart on no data within 10 seconds

--- src/conn_manager.erl
+++ src/conn_manager.erl
@@ -3,31 +3,57 @@
 -compile(export_all).
 
 proc(init, P) ->
-  {ok, _} = gun:open("www.bitmex.com", 443, #{protocols => [http], transport => tls}),
-  CurrentTime = get_timestamp(),
-  InitialState = {[], timer_restart(), CurrentTime},
+  {ok, _}      = gun:open("www.bitmex.com", 443, #{protocols => [http], transport => tls}),
+  InitialState = {[], timer_restart(), get_timestamp()},
   {ok, P#pi{state = InitialState}};
-proc({gun_ws, _, _, Msg}, #pi{state = {Conn, Timer, _}} = P) ->
-  CurrentTime = get_timestamp(),
-%%  io:format("TIC msg: ~p, ~p~n", [Msg, CurrentTime]),
-  {reply, [], P#pi{state = {Conn, Timer, CurrentTime}}};
+
 proc({gun_down, _, _, Reason, _, _}, P) ->
   io:format("TIC DOWN: ~p~n", [Reason]),
   {reply, [], P};
-proc({gun_up, Conn, _}, P) ->
+
+proc({gun_up, Conn, _}, #pi{state = {_, Timer, _}} = P) ->
+  io:format("TIC UP: ~p~n", [Conn]),
   gun:ws_upgrade(Conn, "/realtime"),
-  {reply, [], P};
+  erlang:cancel_timer(Timer),
+  {reply, [], P#pi{state={Conn, timer_restart(), get_timestamp()}}};
+
 proc({gun_upgrade, Conn, _, _, _}, P) ->
   io:format("TIC WS: ~p~n", [Conn]),
   gun:ws_send(Conn, {text, "{\"op\": \"subscribe\", \"args\": [\"orderBookL2:XBTUSD\"]}"}),
   {reply, [], P};
-proc({timer, ping}, #pi{state = {Conn, _, PrevTime}} = P) ->
+
+proc({gun_error, _, _, Reason}, P) ->
+  io:format("TIC ERR: ~p~n", [Reason]),
+  {reply, [], P};
+
+proc({gun_ws, _, _, Msg}, #pi{state = {Conn, Timer, _}} = P) ->
+%%  io:format("TIC msg: ~p, ~p~n", [Msg, CurrentTime]),
+  {reply, [], P#pi{state = {Conn, Timer, get_timestamp()}}};
+
+proc({timer, ping}, #pi{state = {Conn, Timer, PrevTime}} = P) ->
   CurrentTime = get_timestamp(),
-  io:format("TIC prev: ~p, current: ~p~n", [PrevTime, CurrentTime]),
-  {reply, [], P#pi{state = {Conn, timer_restart(), PrevTime}}};
+  if
+    CurrentTime - PrevTime > 10000 ->
+      io:format("TIC TIMEOUT: ~p~n", [CurrentTime - PrevTime]),
+      case Conn of
+        [] ->
+          do_nothing;
+        X when erlang:is_pid(X) ->
+          gun:shutdown(Conn),
+          #pi{name = Name, table = Tab} = P,
+          n2o_pi:stop(Tab, Name)
+      end,
+      {reply, [], P#pi{state = {[], Timer, PrevTime}}};
+    true ->
+      io:format("TIC TS: prev: ~p, current: ~p, diff: ~p~n", [PrevTime, CurrentTime, CurrentTime - PrevTime]),
+      {reply, [], P#pi{state = {Conn, timer_restart(), PrevTime}}}
+  end;
+
 proc(Unknown, P) ->
   io:format("TIC UNK: ~p~n", [Unknown]),
   {reply, [], P}.
+
+% Utilities
 
 get_timestamp() ->
   {Mega, Sec, Micro} = os:timestamp(),
