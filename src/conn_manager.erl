87b3dc4e6713aa217b1676eb8b245dc1ac7afab2
Alexander Temerev <sorhed@gmail.com>
2019-09-01 22:09:24 +0200

    Json parsing, table dispatch

--- src/conn_manager.erl
+++ src/conn_manager.erl
@@ -3,56 +3,69 @@
 -compile(export_all).
 
 proc(init, P) ->
-    {ok, Conn} = gun:open("www.bitmex.com", 443, #{protocols => [http], transport => tls}),
-    io:format("INIT~n"),
-    {ok, P};
+  {ok, _} = gun:open("www.bitmex.com", 443, #{protocols => [http], transport => tls}),
+  io:format("INIT~n"),
+  {ok, P};
 
-proc({gun_ws, _, _, Msg}, #pi{state={A,B,C}}=P) ->
-    io:format("~s",["."]),
-    {reply, [], P#pi{state={A,B,stamp()}}};
+proc({gun_ws, _, _, Msg}, #pi{state = {Conn, Timer, _}} = P) ->
+  process_bitmex_message(Msg),
+  {reply, [], P#pi{state = {Conn, Timer, stamp()}}};
 
-proc({gun_down, _, _, Reason, _, _}, P) ->
-    io:format("TIC DOWN: ~p~n", [Reason]),
-    {reply, [], #pi{state = {[], [], []}}};
+proc({gun_down, _, _, Reason, _, _}, _) ->
+  io:format("TIC DOWN: ~p~n", [Reason]),
+  {reply, [], #pi{state = {[], [], []}}};
 
-proc({gun_up, Conn, _}, #pi{state = {_, Timer, _}} = P) ->
-    io:format("TIC UP: ~p~n", [Conn]),
-    gun:ws_upgrade(Conn, <<"/realtime">>, []),
-    {reply, [], P};
+proc({gun_up, Conn, _}, P) ->
+  io:format("TIC UP: ~p~n", [Conn]),
+  gun:ws_upgrade(Conn, <<"/realtime">>, []),
+  {reply, [], P};
 
-proc({gun_upgrade, Conn, _, _, _}, #pi{ state = { [], [], [] }} = P) ->
-    io:format("TIC WS: ~p~n", [Conn]),
-    gun:ws_send(Conn, {text, "{\"op\": \"subscribe\", \"args\": [\"orderBookL2:XBTUSD\"]}"}),
-    {reply, [], P#pi{state = {Conn,timer_restart(), stamp()}}};
+proc({gun_upgrade, Conn, _, _, _}, #pi{state = {[], [], []}} = P) ->
+  io:format("TIC WS: ~p~n", [Conn]),
+  subscribe(Conn),
+  {reply, [], P#pi{state = {Conn, timer_restart(), stamp()}}};
 
-proc({gun_upgrade, Conn, _, _, _}, #pi{ state = { _, Timer, _ }} = P) ->
-    erlang:cancel_timer(Timer),
-    io:format("TIC WS: ~p~n", [Conn]),
-    gun:ws_send(Conn, {text, "{\"op\": \"subscribe\", \"args\": [\"orderBookL2:XBTUSD\"]}"}),
-    {reply, [], P#pi{state = {Conn,timer_restart(), stamp()}}};
+proc({gun_upgrade, Conn, _, _, _}, #pi{state = {_, Timer, _}} = P) ->
+  erlang:cancel_timer(Timer),
+  io:format("TIC WS: ~p~n", [Conn]),
+  subscribe(Conn),
+  {reply, [], P#pi{state = {Conn, timer_restart(), stamp()}}};
 
-proc({gun_error, Conn, _, Reason}, P) ->
-    io:format("TIC ERR: ~p~n", [Reason]),
-    {reply, [], P};
+proc({gun_error, _, _, Reason}, P) ->
+  io:format("TIC ERR: ~p~n", [Reason]),
+  {reply, [], P};
 
 proc({timer, ping}, #pi{state = {[], [], []} = P}) ->
-    {reply, [], P};
+  {reply, [], P};
 
-proc({timer, ping}, #pi{state = {Conn, Timer, PrevTime},  name= Name, table = Tab} = P) ->
-    erlang:cancel_timer(Timer),
-    D = stamp() - PrevTime,
-    case D > 10000 of
-         true -> spawn(fun() -> n2o_pi:restart(caching,"bitmex") end);
-         false -> io:format("~p~n",[D]) end,
-    {reply, [], P#pi{state = {Conn, timer_restart(), PrevTime}}};
+proc({timer, ping}, #pi{state = {Conn, Timer, PrevTime}} = P) ->
+  erlang:cancel_timer(Timer),
+  D = stamp() - PrevTime,
+  case D > 10000 of
+    true -> spawn(fun() -> n2o_pi:restart(caching, "bitmex") end);
+    false -> io:format("~p~n", [D]) end,
+  {reply, [], P#pi{state = {Conn, timer_restart(), PrevTime}}};
 
-proc(Unknown, #pi{ state = {Conn,_,_}} = P) ->
-    io:format("TIC UNK: ~p~n", [Unknown]),
-    {reply, [], P}.
+proc(Unknown, P) ->
+  io:format("TIC UNK: ~p~n", [Unknown]),
+  {reply, [], P}.
 
 stamp() ->
-    {Mega, Sec, Micro} = os:timestamp(),
-    (Mega * 1000000 + Sec) * 1000 + round(Micro / 1000).
+  {Mega, Sec, Micro} = os:timestamp(),
+  (Mega * 1000000 + Sec) * 1000 + round(Micro / 1000).
 
 timer_restart() ->
-    erlang:send_after(1000, self(), {timer, ping}).
+  erlang:send_after(1000, self(), {timer, ping}).
+
+process_bitmex_message(Msg) ->
+  {text, Content} = Msg,
+  Data = jsone:decode(Content),
+  MaybeTable = maps:find(<<"table">>, Data),
+  case MaybeTable of
+    {ok, <<"orderBookL2">>} -> io:format("~s", ["."]);
+    {ok, <<"trade">>} -> io:format("~s", ["|"]);
+    _ -> do_nothing
+  end.
+
+subscribe(Conn) ->
+  gun:ws_send(Conn, {text, "{\"op\": \"subscribe\", \"args\": [\"orderBookL2:XBTUSD\", \"trade:XBTUSD\"]}"}).
