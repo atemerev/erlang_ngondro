f25d1b1c33f2681d0f7367846a0363d32b639c6c
Alexander Temerev <sorhed@gmail.com>
2019-09-02 23:11:24 +0200

    Orderbook assembly fixes

--- src/orderbook.erl
+++ src/orderbook.erl
@@ -1,55 +1,38 @@
 -module(orderbook).
 -author("atemerev").
 -include("state.hrl").
--export([new_book/1, insert/2, delete/3, update/5]).
+-export([new_book/1, insert/2, delete/3, update/5, best/1]).
 
 new_book(Symbol) ->
   #orderbook{symbol = Symbol, bids = [], offers = []}.
 
-insert(Book = #orderbook{bids = Bids, offers = Offers}, Entry = #order_entry{side = Side, price = Price}) ->
-  {Line, Fun} = case Side of
-                  bid -> {Bids, fun(X) -> Price < X#order_entry.price end};
-                  offer -> {Offers, fun(X) -> Price < X#order_entry.price end};
-                  _ -> {unknown, invalid_entry_side}
-                end,
-
+insert(Book = #orderbook{}, Entry = #order_entry{side = Side, price = Price}) ->
+  Line = get_line(Book, Side),
+  Fun = case Side of
+          bid -> fun(X) -> Price > X#order_entry.price end;
+          offer -> fun(X) -> Price < X#order_entry.price end;
+          _ -> invalid_entry_side
+        end,
   MaybeIdx = fputils:find_index(Line, Fun),
   NewLine = case MaybeIdx of
               not_found -> lists:append(Line, [Entry]);
               Idx -> fputils:insert_at(Line, Idx, Entry)
             end,
+  put_line(Book, Side, NewLine).
 
-  case Side of
-    bid -> Book#orderbook{bids = NewLine};
-    offer -> Book#orderbook{offers = NewLine};
-    _ -> invalid_entry_side
-  end.
-
-delete(Book = #orderbook{bids = Bids, offers = Offers}, Side, Id) ->
-  Line = case Side of
-           bid -> Bids;
-           offer -> Offers;
-           _ -> invalid_entry_side
-         end,
-  MaybeIdx = fputils:find_index(Line, fun(X) -> Id == X end),
+delete(Book = #orderbook{}, Side, Id) ->
+  Line = get_line(Book, Side),
+  MaybeIdx = fputils:find_index(Line, fun(X) -> X#order_entry.id == Id end),
   NewLine = case MaybeIdx of
               not_found -> Line;
               Idx -> fputils:delete_at(Line, Idx)
             end,
-  case Side of
-    bid -> Book#orderbook{bids = NewLine};
-    offer -> Book#orderbook{offers = NewLine};
-    _ -> invalid_entry_side
-  end.
+  put_line(Book, Side, NewLine).
 
 update(Book = #orderbook{}, Side, Id, _, 0) -> delete(Book, Side, Id);
 
-update(Book = #orderbook{bids = Bids, offers = Offers}, Side, Id, MaybeNewPrice, MaybeNewAmount) ->
-  Line = case Side of
-           bid -> Bids;
-           offer -> Offers;
-           _ -> invalid_entry_side
-         end,
+update(Book = #orderbook{}, Side, Id, MaybeNewPrice, MaybeNewAmount) ->
+  Line = get_line(Book, Side),
   MaybeIdxById = fputils:find_index(Line, fun(X) -> X#order_entry.id == Id end),
 
   case MaybeIdxById of
@@ -65,10 +48,30 @@ update(Book = #orderbook{bids = Bids, offers = Offers}, Side, Id, MaybeNewPrice,
              _ -> E1
            end,
       NewLine = fputils:replace_at(Line, Idx, E2),
-      case Side of
-        bid -> Book#orderbook{bids = NewLine};
-        offer -> Book#orderbook{offers = NewLine};
-        _ -> invalid_entry_side
-      end
+      put_line(Book, Side, NewLine)
   end.
 
+get_line(_ = #orderbook{bids = Bids, offers = Offers}, Side) ->
+  case Side of
+    bid -> Bids;
+    offer -> Offers;
+    _ -> invalid_entry_side
+  end.
+
+put_line(Book = #orderbook{}, Side, NewLine) ->
+  case Side of
+    bid -> Book#orderbook{bids = NewLine};
+    offer -> Book#orderbook{offers = NewLine};
+    _ -> invalid_entry_side
+  end.
+
+best(_ = #orderbook{bids = Bids, offers = Offers}) ->
+  BestBid = case Bids of
+              [] -> not_found;
+              [BH | _] -> BH#order_entry.price
+            end,
+  BestOffer = case Offers of
+                [] -> not_found;
+                [OH | _] -> OH#order_entry.price
+              end,
+  {BestBid, BestOffer}.
