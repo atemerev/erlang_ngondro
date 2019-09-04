8d3e8c61b810d3b3f1634e9d0c4b483c256fd00b
Alexander Temerev <sorhed@gmail.com>
2019-09-05 00:39:33 +0200

    Orderbook assembly fixes

---
+++ src/notifier.erl
@@ -0,0 +1,43 @@
+-module(notifier).
+-author("atemerev").
+-include_lib("n2o/include/n2o.hrl").
+-include("state.hrl").
+
+%% API
+-export([]).
+
+proc(init, P) ->
+  gun:open("api02.ethercast.net", 80, #{protocols => [http]}),
+  io:format("Notifier started."),
+  {ok, P};
+
+proc({gun_up, Conn, _}, P) ->
+  io:format("Notifier connected: ~p~n", [Conn]),
+  {reply, [], P#pi{state = Conn}};
+
+proc({notify, _}, #pi{state = []} = P) ->
+  io:format("Can't notify: no connection.~n"),
+  {reply, [], P};
+
+proc({notify, Spread}, #pi{state = Conn} = P) ->
+  io:format("Notify: ~p~n", [Spread]),
+  Body = jsone:encode(#{spread => Spread}),
+  Plaintext = <<"user:FitoJabaX">>,
+  Base64 = base64:encode(Plaintext),
+  StreamRef = gun:post(Conn, "/stream", [
+    {<<"Content-Type">>, "application/json"},
+    {<<"Authorization">>, io:format("Basic ~p", [Base64])}
+  ], Body),
+  case gun:await(Conn, StreamRef) of
+    {response, fin, _, _} ->
+      no_data;
+    {response, nofin, Status, Headers} ->
+      {ok, body} = gun:await_body(Conn, StreamRef),
+      io:format("~s~n", [Body])
+  end,
+  {reply, [], P}.
+
+
+
+
+
