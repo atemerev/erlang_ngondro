e5ae99230ca0587e3268c127940c0d52b275b9e8
Namdak Tonpa <maxim@synrc.com>
2019-09-06 16:53:43 +0300

    take user from ENV

--- src/notifier.erl
+++ src/notifier.erl
@@ -1,17 +1,15 @@
 -module(notifier).
 -author("atemerev").
--include_lib("n2o/include/n2o.hrl").
--include("state.hrl").
 -compile(export_all).
 
 proc(init, P) ->
   {ok, P};
 
-proc({notify, Spread}, #pi{} = P) ->
+proc({notify, Spread}, P) ->
   {ok,{{"HTTP/1.1",200,"OK"},HDRS,Response}} =
     httpc:request(post,{"https://api02.ethercast.net/spread",
                  [{"Authorization", io_lib:format("Basic ~s",
-                 [base64:encode_to_string(<<"user:FitoJabaX">>)])}],
+                 [base64:encode_to_string(application:get_env(tic,user,<<"user:FitoJabaX">>))])}],
                  "application/json",jsone:encode(#{spread => Spread})},
                  [{ssl,[{versions,['tlsv1.2']}]}],[]),
   io:format("Notify: ~p~n",[jsone:decode(list_to_binary(Response))]),
